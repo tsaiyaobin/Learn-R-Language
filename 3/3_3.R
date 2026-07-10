f57cb99d4e3fbe4f7f1b825df00b6675910c2337
tsaiyaobin <austin52012118@gmail.com>
2026-07-10 21:34:41 +0800

    update 3_3

--- 3/3_3.R
+++ 3/3_3.R
@@ -3,3 +3,19 @@ accounts <- data.frame(state = c("PA", "CA", "MA"),
                        income = c(1000, 1100, 900),
                        married = c(T, F, F))
 accounts
+## 看屬性（欄位名、列名、class）
+attributes(accounts) 
+## 每欄的型態、內容概覽
+str(accounts)
+
+## matrix 轉 dataframe
+mat <- matrix(data = 1:35, nrow = 5, ncol = 7)  # 欄、列都是[1,] [2,]...
+mat
+dat <- as.data.frame(mat)                       # 有欄名(v1,v2...) 與 列號(1,2...)
+dat
+
+str(dat) # 5 obs. of  7 variables (5 筆觀測值, 7個變數)
+dat$V1 # 等價於 dat[,1]
+
+
+
