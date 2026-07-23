36779444c0e3b3e14cf4fd9a01d3a5102e2b8703
tsaiyaobin <austin52012118@gmail.com>
2026-07-23 08:56:04 +0800

    update 0722

---
+++ 5/5_1.R
@@ -0,0 +1,16 @@
+data<-read.delim("MLL_train.txt")
+dim(data)
+data[1:3,]
+# 把列名稱從 1,2,...,12582 改成第 1 col 的名稱
+rownames(data) <- data[,1]
+
+data1 <- read.delim("MLL_train.txt", row.names=1)
+# 用這個邏輯向量去篩選 1:nrow(data)（也就是列的索引 1, 2, 3, ..., nrow(data)，
+# 只留下對應 TRUE 的位置，回傳的是列的位置（第幾列），不是數量。
+(1:nrow(data))[row.names(data) == "AFFX-DapX-3_at"]
+
+grep("AFFX", rownames(data))
+#### correlation matrix ####
+data2 <- data1[,-1]
+cor1 <- cor(data2)
+cor2 <- cor(t(data2))
